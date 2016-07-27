27c231990df360fd04b5e7f941cfebc3c58ed808
bimcreator <bimc.develop@gmail.com>
2016-07-27 16:36:32 +0400

    Add PerActivity

---
+++ app/src/main/java/com/bashrexeader/bashrxeader/injection/PerActivity.java
@@ -0,0 +1,16 @@
+package com.bashrexeader.bashrxeader.injection;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Davey on 27.07.2016.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerActivity {
+
+
+}
