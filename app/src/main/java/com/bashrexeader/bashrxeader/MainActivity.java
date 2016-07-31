bd83fbf3e6e4b7f4491853db800b843811d22399
bimcreator <bimc.develop@gmail.com>
2016-07-31 16:58:04 +0400

    Little trick with .map() chaining and get hashCode input String

--- app/src/main/java/com/bashrexeader/bashrxeader/MainActivity.java
+++ app/src/main/java/com/bashrexeader/bashrxeader/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Observable.just("Text")
                 .map(s -> s + " -Owner")
+                .map(s -> s.hashCode())
+                .map(i -> Integer.toString(i))
                 .subscribe(s -> System.out.println(s));
     }
 
