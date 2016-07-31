fdff8b23a9e33554d56af2bdfb8a6a2a0f84e839
bimcreator <bimc.develop@gmail.com>
2016-07-31 15:42:29 +0400

    Implements Observer and Subscriber

--- app/src/main/java/com/bashrexeader/bashrxeader/MainActivity.java
+++ app/src/main/java/com/bashrexeader/bashrxeader/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bashrexeader.bashrxeader;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myObservable.subscribe(mySubcriber);
     }
+
+    Observable<String> myObservable = Observable.create(
+            new Observable.OnSubscribe<String>() {
+                @Override
+                public void call(Subscriber<? super String> subscriber) {
+                    subscriber.onNext("text");
+                    subscriber.onCompleted();
+                }
+            }
+    );
+
+    Subscriber<String> mySubcriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            System.out.println(s);
+        }
+    };
+
 }
